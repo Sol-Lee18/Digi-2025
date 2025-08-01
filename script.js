7c5af301cb284735222ed2d2b7871f45e00cf3ff
Sol <22336@my.westlakegirls.school.nz>
2025-08-01 15:15:34 +1200

    Editor change 3 (js)

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+let optionsButtons = document.querySelectorAll(".optionButton");
+let fontOptionsButton = document.querySelectorAll(".fontOptionsButton");
+let formatButtons = document.querySelectorAll(".format");
+
+const initializer = function() {
+  highlighter(formatButtons, false);
+}
+
+const highlighter = (className, needsRemoval) => {
+  className.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (needsRemoval) {
+        let alreadyActive = false;
+
+        if (button.classList.contains("active")) {
+          alreadyActive = true;
+        }
+
+        highligherRemover(className);
+        if(!alreadyActive) {
+          button.classList.add("active");
+        }
+      } else {
+        button.classList.toggle("active");
+      }
+    });
+  });
+};
+
+window.on
+
+
+
+
 const textArea = document.getElementById('text-area');
 const wordCountDisplay = document.getElementById('word-count');
 
